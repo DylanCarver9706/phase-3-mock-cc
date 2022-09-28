a13e51d020dec7db93d64808778b167d77a3358a
Dylan <dylancarver14@gmail.com>
2022-09-28 10:42:51 -0500

    done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,11 @@
+require 'pry'
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def recieved_one?(item_name)
+        binding.pry
+        self.freebies.any? do |freebie|
+        freebie.item_name == item_name
+    end
 end
